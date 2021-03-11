857ea084abeb18a01a918add8eb09496d0739f95
jmolina518 <jmolina518@gmail.com>
2021-03-10 19:07:56 -0600

    add Nacho as a contact

--- src/data.js
+++ src/data.js
@@ -2,36 +2,43 @@ export default [
   {
     id: 1,
     name: 'Julio Molina',
-    timezone: 'U.S. Central Time',
+    timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1369130797450559489/Ap-WRzpK_400x400.jpg',
   },
   {
     id: 2,
     name: 'Rocco Sangellino',
-    timezone: 'U.S. Mountain Time',
+    timezone: 'America/Los_Angeles',
     image:
       'https://pbs.twimg.com/profile_images/1369046630779908098/gjQqL1dH_400x400.jpg',
   },
   {
     id: 3,
     name: 'Shaquil Hansford',
-    timezone: 'U.S. Eastern Time',
+    timezone: 'America/Detroit',
     image:
       'https://pbs.twimg.com/profile_images/1363959225483943939/5CMcxvCo_400x400.jpg',
   },
   {
     id: 4,
     name: 'James Cox',
-    timezone: 'U.S. Central Time',
+    timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1239375276209065986/jH1y6zcd_400x400.jpg',
   },
   {
     id: 5,
     name: 'Charles Desider',
-    timezone: 'U.S. Central Time',
+    timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1233168090797629444/XtZK9MW7_400x400.jpg',
+  },
+  {
+    id: 6,
+    name: 'Nacho Iacovino',
+    timezone: 'Europe/Madrid',
+    image:
+      'https://pbs.twimg.com/profile_images/1352285231030689794/MgpWv4q0_400x400.jpg',
   },
 ]
