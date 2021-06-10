33df4b9cd851be10a0b308e73a3ddf32a676960f
jmolina518 <jmolina518@gmail.com>
2021-06-09 22:55:47 -0500

    uupdate to hide timezone

--- src/List.js
+++ src/List.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import DateTime from './components/DateTime'
+import React from 'react';
+import DateTime from './components/DateTime';
 
 const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        const { id, name, image, timezone } = person
+        const { id, name, image, timezone } = person;
         return (
           <article key={id} className='person'>
             <img src={image} alt={name} />
             <div>
               <h4>{name}</h4>
-              <p>{timezone}</p>
+              {/* <p>{timezone}</p> */}
               <div>
                 <DateTime timezone={timezone} />
               </div>
             </div>
           </article>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
 
-export default List
+export default List;
--- src/data.js
+++ src/data.js
@@ -29,76 +29,83 @@ const people = [
   },
   {
     id: 5,
+    name: 'Tom Burdak',
+    timezone: 'America/Los_Angeles',
+    image:
+      'https://pbs.twimg.com/profile_images/1317264333378867201/B3C4wxh9_400x400.jpg',
+  },
+  {
+    id: 6,
     name: 'James Cox',
     timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1400123295408742402/QqrAW2K3_400x400.jpg',
   },
   {
-    id: 6,
+    id: 7,
     name: 'Charles Desiderio',
     timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1233168090797629444/XtZK9MW7_400x400.jpg',
   },
   {
-    id: 7,
+    id: 8,
     name: 'Nacho Iacovino',
     timezone: 'Europe/Madrid',
     image:
       'https://pbs.twimg.com/profile_images/1352285231030689794/MgpWv4q0_400x400.jpg',
   },
   {
-    id: 8,
+    id: 9,
     name: 'Edan Ben-Atar',
     timezone: 'America/New_York',
     image:
       'https://pbs.twimg.com/profile_images/1378010377326317569/tCFiHILJ_400x400.jpg',
   },
   {
-    id: 9,
+    id: 10,
     name: 'Sarah Shook',
     timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1400216053280559104/WvAf6G1M_400x400.jpg',
   },
   {
-    id: 10,
+    id: 11,
     name: 'Kelsi Proulx',
     timezone: 'America/Los_Angeles',
     image:
       'https://pbs.twimg.com/profile_images/1369160136141377539/biO8qtON_400x400.jpg',
   },
   {
-    id: 11,
+    id: 12,
     name: 'Freddie Carthy',
     timezone: 'America/Los_Angeles',
     image:
       'https://pbs.twimg.com/profile_images/1401420077837131776/px6RT7a3_400x400.jpg',
   },
   {
-    id: 12,
+    id: 13,
     name: 'Vlad Pasca',
     timezone: 'Europe/Bucharest',
     image:
       'https://pbs.twimg.com/profile_images/1377357987359100936/uptxZfmS_400x400.jpg',
   },
   {
-    id: 13,
+    id: 14,
     name: 'Andres Contreras',
     timezone: 'Europe/Stockholm',
     image:
       'https://pbs.twimg.com/profile_images/1382922347649368064/djn2wQlh_400x400.jpg',
   },
   {
-    id: 14,
+    id: 15,
     name: 'Mark Freeman',
     timezone: 'Europe/London',
     image:
       'https://pbs.twimg.com/profile_images/1369054987792289797/fLyFm_XL_400x400.jpg',
   },
   {
-    id: 15,
+    id: 16,
     name: 'Laura Todd',
     timezone: 'Europe/London',
     image:
