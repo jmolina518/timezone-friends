e2ee648398950391722f767901827e373f0dfa45
jmolina518 <jmolina518@gmail.com>
2021-06-09 22:39:57 -0500

    add new friends

--- src/data.js
+++ src/data.js
@@ -15,76 +15,90 @@ const people = [
   },
   {
     id: 3,
+    name: 'Katherine Peterson',
+    timezone: 'America/Los_Angeles',
+    image:
+      'https://pbs.twimg.com/profile_images/1354163904213966848/4dOl8di8_400x400.jpg',
+  },
+  {
+    id: 4,
     name: 'Shaquil Hansford',
     timezone: 'America/Detroit',
     image:
       'https://pbs.twimg.com/profile_images/1387992635105812485/54yCv-mQ_400x400.jpg',
   },
   {
-    id: 4,
+    id: 5,
     name: 'James Cox',
     timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1400123295408742402/QqrAW2K3_400x400.jpg',
   },
   {
-    id: 5,
+    id: 6,
     name: 'Charles Desiderio',
     timezone: 'America/Chicago',
     image:
       'https://pbs.twimg.com/profile_images/1233168090797629444/XtZK9MW7_400x400.jpg',
   },
   {
-    id: 6,
+    id: 7,
     name: 'Nacho Iacovino',
     timezone: 'Europe/Madrid',
     image:
       'https://pbs.twimg.com/profile_images/1352285231030689794/MgpWv4q0_400x400.jpg',
   },
   {
-    id: 7,
+    id: 8,
     name: 'Edan Ben-Atar',
     timezone: 'America/New_York',
     image:
       'https://pbs.twimg.com/profile_images/1378010377326317569/tCFiHILJ_400x400.jpg',
   },
   {
-    id: 8,
+    id: 9,
+    name: 'Sarah Shook',
+    timezone: 'America/Chicago',
+    image:
+      'https://pbs.twimg.com/profile_images/1400216053280559104/WvAf6G1M_400x400.jpg',
+  },
+  {
+    id: 10,
     name: 'Kelsi Proulx',
     timezone: 'America/Los_Angeles',
     image:
       'https://pbs.twimg.com/profile_images/1369160136141377539/biO8qtON_400x400.jpg',
   },
   {
-    id: 9,
+    id: 11,
     name: 'Freddie Carthy',
     timezone: 'America/Los_Angeles',
     image:
-      'https://pbs.twimg.com/profile_images/1338009004866355200/XmU0LBJ1_400x400.jpg',
+      'https://pbs.twimg.com/profile_images/1401420077837131776/px6RT7a3_400x400.jpg',
   },
   {
-    id: 10,
+    id: 12,
     name: 'Vlad Pasca',
     timezone: 'Europe/Bucharest',
     image:
       'https://pbs.twimg.com/profile_images/1377357987359100936/uptxZfmS_400x400.jpg',
   },
   {
-    id: 11,
+    id: 13,
     name: 'Andres Contreras',
     timezone: 'Europe/Stockholm',
     image:
       'https://pbs.twimg.com/profile_images/1382922347649368064/djn2wQlh_400x400.jpg',
   },
   {
-    id: 12,
+    id: 14,
     name: 'Mark Freeman',
     timezone: 'Europe/London',
     image:
       'https://pbs.twimg.com/profile_images/1369054987792289797/fLyFm_XL_400x400.jpg',
   },
   {
-    id: 13,
+    id: 15,
     name: 'Laura Todd',
     timezone: 'Europe/London',
     image:
